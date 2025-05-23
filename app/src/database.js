2247b88c7b5727aa12d0937407c4ef98f1bc5806
Jonathan-Lorfils <70392748+Jonathan-Lorfils@users.noreply.github.com>
2025-05-22 21:10:17 -0400

    Update docker files with postgresql

--- app/src/database.js
+++ app/src/database.js
@@ -1,8 +1,9 @@
 
 import { Sequelize } from 'sequelize';
 
-const databaseURL = 'postgres://postgres:XRoot123@localhost:5432/postgres'
+const databaseURL = process.env.DATABASE_URL || 'postgres://postgres:XRoot123@localhost:5432/postgres'
 const sequelize = new Sequelize(databaseURL, {
+    dialect: 'postgres',
     logging: false
 })
 
